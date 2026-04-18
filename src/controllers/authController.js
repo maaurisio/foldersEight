75780a1d20235e2f07928fa4e172b432538f238f
maaurisio <mauriciomrla@gmail.com>
2026-04-17 22:49:50 -0500

    txt actulizado authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const authService = require('../services/authService');
+const blacklistRepository = require('../repositories/tokenBlackList');
 
 async function register(req, res) {
     console.log(req.body);
@@ -19,9 +20,24 @@ async function login(req, res) {
         res.status(400).json({ error: error.message });
     }
 }
+//logout
+async function logout(req, res) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Token no proporcionado' });
+    }
+    const token = authHeader.split(' ')[1];
 
+    if (!token) {
+        return res.status(401).json({ error: 'Token no proporcionado' });
+    }
+    console.log("¿Qué tiene el repositorio?:", blacklistRepository);
+    await blacklistRepository.agregarToken(token);
+    res.status(200).json({ message: 'Logout exitoso' });
+}
 
 module.exports = {
     register,
-    login
+    login,
+    logout
 };
